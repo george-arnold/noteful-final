f140936f9bdb30520f6ef873885f4163bad09d42
George Arnold <george.arnold.88@gmail.com>
2020-02-25 09:58:22 -0500

    fixing config to correct web address

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-  API_ENDPOINT: "https://git.heroku.com/sheltered-tor-93215.git",
+  API_ENDPOINT: "https://sheltered-tor-93215.herokuapp.com/",
   API_KEY: process.env.REACT_APP_API_KEY
 };
