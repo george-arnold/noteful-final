599cb6bb057ee59c4fe05180855147fa62e1fb99
George Arnold <george.arnold.88@gmail.com>
2020-02-26 10:15:02 -0500

    updated case sensitive data transfer

--- src/notes-helpers.js
+++ src/notes-helpers.js
@@ -1,15 +1,11 @@
+export const findFolder = (folders = [], folderid) =>
+  folders.find(folder => folder.id === Number(folderid));
 
-export const findFolder = (folders=[], folderId) =>
-  folders.find(folder => folder.id === folderId)
+export const findNote = (notes = [], noteid) =>
+  notes.find(note => note.id === Number(noteid));
 
-export const findNote = (notes=[], noteId) =>
-  notes.find(note => note.id === noteId)
+export const getNotesForFolder = (notes = [], folderid) =>
+  !folderid ? notes : notes.filter(note => note.folderid === Number(folderid));
 
-export const getNotesForFolder = (notes=[], folderId) => (
-  (!folderId)
-    ? notes
-    : notes.filter(note => note.folderId === folderId)
-)
-
-export const countNotesForFolder = (notes=[], folderId) =>
-  notes.filter(note => note.folderId === folderId).length
+export const countNotesForFolder = (notes = [], folderid) =>
+  notes.filter(note => note.folderid === Number(folderid)).length;
